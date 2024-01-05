64f10988f9f6f13a7fc1d0f8b72ea88fc8d68080
Ankit Kaushik <ankitkaushikx@gmail.com>
2024-01-05 11:49:41 +0530

    Intial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
